c752e677ec17c530b3d417da48156d402c8c2471
zein12 <jena.zein@gmail.com>
2017-11-14 21:33:44 +0700

    Update http_client.py

--- linebot/http_client.py
+++ linebot/http_client.py
@@ -222,3 +222,230 @@ class RequestsHttpResponse(HttpResponse):
         :param bool decode_unicode:
         """
         return self.response.iter_content(chunk_size=chunk_size, decode_unicode=decode_unicode)
+    
+    def post_content(self, urls, data=None, files=None):
+        return self._session.post(urls, headers=self._headers, data=data, files=files)
+
+    """Image"""
+
+    
+    def sendImage(self, to_, path):
+        M = Message(to=to_, text=None, contentType = 1)
+        M.contentMetadata = None
+        M.contentPreview = None
+        M2 = self._client.sendMessage(0,M)
+        M_id = M2.id
+        files = {
+            'file': open(path, 'rb'),
+        }
+        params = {
+            'name': 'media',
+            'oid': M_id,
+            'size': len(open(path, 'rb').read()),
+            'type': 'image',
+            'ver': '1.0',
+        }
+        data = {
+            'params': json.dumps(params)
+        }
+        r = self.post_content('https://obs-sg.line-apps.com/talk/m/upload.nhn', data=data, files=files)
+        if r.status_code != 201:
+            raise Exception('Upload image failure.')
+        return True
+
+    
+    def sendImageWithURL(self, to_, url):
+        path = '%s/pythonLine-%i.data' % (tempfile.gettempdir(), randint(0, 9))
+        r = requests.get(url, stream=True)
+        if r.status_code == 200:
+            with open(path, 'w') as f:
+                shutil.copyfileobj(r.raw, f)
+        else:
+            raise Exception('Download image failure.')
+        try:
+            self.sendImage(to_, path)
+        except Exception as e:
+            raise e
+    """User"""
+
+    
+    def getProfile(self):
+        return self._client.getProfile()
+
+    
+    def getSettings(self):
+        return self._client.getSettings()
+
+    
+    def getUserTicket(self):
+        return self._client.getUserTicket()
+
+    
+    def updateProfile(self, profileObject):
+        return self._client.updateProfile(0, profileObject)
+
+    
+    def updateSettings(self, settingObject):
+        return self._client.updateSettings(0, settingObject)
+
+    """Operation"""
+
+    
+    def fetchOperation(self, revision, count):
+        return self._client.fetchOperations(revision, count)
+
+    
+    def getLastOpRevision(self):
+        return self._client.getLastOpRevision()
+
+    """Message"""
+
+    
+    def sendEvent(self, messageObject):
+        return self._client.sendEvent(0, messageObject)
+
+    
+    def sendMessage(self, messageObject):
+        return self._client.sendMessage(0,messageObject)
+
+    def getLastReadMessageIds(self, chatId):
+        return self._client.getLastReadMessageIds(0,chatId)
+
+    """Image"""
+
+    
+    def post_content(self, url, data=None, files=None):
+        return self._session.post(url, headers=self._headers, data=data, files=files)
+
+    """Contact"""
+
+    
+    def blockContact(self, mid):
+        return self._client.blockContact(0, mid)
+
+    
+    def unblockContact(self, mid):
+        return self._client.unblockContact(0, mid)
+
+    
+    def findAndAddContactsByMid(self, mid):
+        return self._client.findAndAddContactsByMid(0, mid)
+
+    
+    def findAndAddContactsByUserid(self, userid):
+        return self._client.findAndAddContactsByUserid(0, userid)
+
+    
+    def findContactsByUserid(self, userid):
+        return self._client.findContactByUserid(userid)
+
+    
+    def findContactByTicket(self, ticketId):
+        return self._client.findContactByUserTicket(ticketId)
+
+    
+    def getAllContactIds(self):
+        return self._client.getAllContactIds()
+
+    
+    def getBlockedContactIds(self):
+        return self._client.getBlockedContactIds()
+
+    
+    def getContact(self, mid):
+        return self._client.getContact(mid)
+
+    
+    def getContacts(self, midlist):
+        return self._client.getContacts(midlist)
+
+    
+    def getFavoriteMids(self):
+        return self._client.getFavoriteMids()
+
+    
+    def getHiddenContactMids(self):
+        return self._client.getHiddenContactMids()
+
+
+    """Group"""
+
+    
+    def acceptGroupInvitation(self, groupId):
+        return self._client.acceptGroupInvitation(0, groupId)
+
+    
+    def acceptGroupInvitationByTicket(self, groupId, ticketId):
+        return self._client.acceptGroupInvitationByTicket(0, groupId, ticketId)
+
+    
+    def cancelGroupInvitation(self, groupId, contactIds):
+        return self._client.cancelGroupInvitation(0, groupId, contactIds)
+
+    
+    def createGroup(self, name, midlist):
+        return self._client.createGroup(0, name, midlist)
+
+    
+    def getGroup(self, groupId):
+        return self._client.getGroup(groupId)
+
+    
+    def getGroups(self, groupIds):
+        return self._client.getGroups(groupIds)
+
+    
+    def getGroupIdsInvited(self):
+        return self._client.getGroupIdsInvited()
+
+    
+    def getGroupIdsJoined(self):
+        return self._client.getGroupIdsJoined()
+
+    
+    def inviteIntoGroup(self, groupId, midlist):
+        return self._client.inviteIntoGroup(0, groupId, midlist)
+
+    
+    def kickoutFromGroup(self, groupId, midlist):
+        return self._client.kickoutFromGroup(0, groupId, midlist)
+
+    
+    def leaveGroup(self, groupId):
+        return self._client.leaveGroup(0, groupId)
+
+    
+    def rejectGroupInvitation(self, groupId):
+        return self._client.rejectGroupInvitation(0, groupId)
+
+    
+    def reissueGroupTicket(self, groupId):
+        return self._client.reissueGroupTicket(groupId)
+
+    
+    def updateGroup(self, groupObject):
+        return self._client.updateGroup(0, groupObject)
+
+    """Room"""
+
+    
+    def createRoom(self, midlist):
+        return self._client.createRoom(0, midlist)
+
+    
+    def getRoom(self, roomId):
+        return self._client.getRoom(roomId)
+
+    
+    def inviteIntoRoom(self, roomId, midlist):
+        return self._client.inviteIntoRoom(0, roomId, midlist)
+
+    
+    def leaveRoom(self, roomId):
+        return self._client.leaveRoom(0, roomId)
+
+    """unknown function"""
+
+    
+    def noop(self):
+return self._client.noop()
